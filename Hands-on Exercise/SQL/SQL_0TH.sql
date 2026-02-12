38535da58cf20ebe6e104be3723df31ee5f21f95
George Emmanuel <george444emmanuel@gmail.com>
2026-02-12 17:12:12 +0530

    Update Feb

---
+++ Hands-on Exercise/SQL/SQL_0TH.sql
@@ -0,0 +1,13 @@
+SQL 
+    1. Introduction
+    2. Query Data (SELECT)
+    3. Data Definition (DDL)
+    4. Data Manipulation (DML)
+    5. Filtering Data
+    6. Combining Data
+    7. Row-Level Functions
+    8. Aggregation & Analytical Functions
+    9. Advanced SQL Techniques
+    10. Performance Optimization
+    11. AI & SQL
+    12. SQL Projects
