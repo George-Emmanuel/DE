4e1f13344b2bd93a7e02aa098fcd6a85adf07b56
George Emmanuel <george444emmanuel@gmail.com>
2026-02-21 22:23:41 +0530

    Update Feb

---
+++ Hands-on Exercise/SQL/OTG.sql
@@ -0,0 +1,11 @@
+-- What is SQL?
+-- SQL stands for Structured Query Language. It is a programming language used to manage and manipulate relational databases. 
+-- SQL allows users to create, read, update, and delete data in a database, as well as perform various operations such as filtering, sorting, and aggregating data. 
+-- It is widely used in various applications, including web development, data analysis, and business intelligence.
+
+-- Why learn SQL?
+-- Learning SQL is essential for anyone working with data, as it is the standard language for interacting with relational databases. It enables you to efficiently retrieve and manipulate data, which is crucial for making informed decisions based on data analysis.
+-- SQL is also a valuable skill in the job market, as many roles in data science, data analysis, and software development require proficiency in SQL.
+-- Additionally, SQL is relatively easy to learn compared to other programming languages, making it accessible for beginners.
+
+-- What
