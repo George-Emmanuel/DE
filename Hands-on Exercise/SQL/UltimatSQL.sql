beed07f0239a51ae11f74343fe293db5917cb08a
George Emmanuel <george444emmanuel@gmail.com>
2026-03-04 19:17:02 +0530

    Update March

--- Hands-on Exercise/SQL/UltimatSQL.sql
+++ Hands-on Exercise/SQL/UltimatSQL.sql
@@ -342,5 +342,7 @@ FROM Sales.Employees;
 -- NULL Functions
 	-- ISNULL: Replaces NULL with a specified replacement value.
 	-- COALESCE: Returns the first non-NULL value from a list of expressions.
+		SELECT AVG(Score) AS Average_Score FROM Sales.Customers;
+		SELECT AVG(ISNULL(Score, 0)) AS Averag_Score_Null_Also FROM Sales.Customers;
 	-- NULLIF: Returns NULL if the two expressions are equal, otherwise returns the first expression.
 	-- IFNULL: Similar to ISNULL, but specific to certain database systems like MySQL
