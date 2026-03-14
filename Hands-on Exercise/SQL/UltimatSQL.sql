cea80782e601c10e24ed1a122c948454aa3d3571
George Emmanuel <george444emmanuel@gmail.com>
2026-03-14 22:48:08 +0530

    Update Feb

--- Hands-on Exercise/SQL/UltimatSQL.sql
+++ Hands-on Exercise/SQL/UltimatSQL.sql
@@ -11,7 +11,8 @@
 -- 	Additionally, SQL is relatively easy to learn compared to other programming languages, making it accessible for beginners.
 
 -- What is Database & Types of Database?
--- 	A database is an organized collection of data that is stored and accessed electronically. It allows users to efficiently manage and retrieve data. There are several types of databases, including:
+-- 	A database is an organized collection of data that is stored and accessed electronically. 
+--		It allows users to efficiently manage and retrieve data. There are several types of databases, including:
 -- 	1. Relational Databases: These databases store data in tables with rows and columns. Examples include MySQL, PostgreSQL, and Oracle.
 -- 	2. NoSQL Databases: These databases are designed for unstructured data and do not use the traditional table-based structure. Examples include MongoDB and Cassandra.
 -- 	3. In-Memory Databases: These databases store data in memory for faster access. Examples include Redis and Memcached.
