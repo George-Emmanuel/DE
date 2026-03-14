b627c8d12f5fb1cebeaaefd54c79f01b1847c47c
George Emmanuel <george444emmanuel@gmail.com>
2026-03-14 22:47:57 +0530

    Update Feb

--- Hands-on Exercise/SQL/UltimatSQL.sql
+++ Hands-on Exercise/SQL/UltimatSQL.sql
@@ -6,7 +6,8 @@
 
 -- Why learn SQL?
 -- 	Learning SQL is essential for anyone working with data, as it is the standard language for interacting with relational databases. It enables you to efficiently retrieve and manipulate data, which is crucial for making informed decisions based on data analysis.
--- 	SQL is also a valuable skill in the job market, as many roles in data science, data analysis, and software development require proficiency in SQL.
+-- 	SQL is also a valuable skill in the job market, as many roles in data science, data analysis, and software development 
+--		require proficiency in SQL.
 -- 	Additionally, SQL is relatively easy to learn compared to other programming languages, making it accessible for beginners.
 
 -- What is Database & Types of Database?
