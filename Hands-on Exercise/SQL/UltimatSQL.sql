bd87d4d8e2b4226b369defe65269c0e044c91a2c
George Emmanuel <george444emmanuel@gmail.com>
2026-03-15 19:17:20 +0530

    Update March

--- Hands-on Exercise/SQL/UltimatSQL.sql
+++ Hands-on Exercise/SQL/UltimatSQL.sql
@@ -915,7 +915,8 @@ FROM Sales.Employees;
 -- COMMON TABLE EXPRESSIONS (CTEs)
 	-- Definition: A Common Table Expression (CTE) is a temporary result set that can be referenced within a 
 	--		SELECT, INSERT, UPDATE, or DELETE statement. 
-	-- It is defined using the WITH clause and can be used to simplify complex queries, improve readability, and enable recursive queries.
+	-- It is defined using the WITH clause and can be used to simplify complex queries, improve readability, and 
+	--		enable recursive queries.
 
 	-- Syntax:
 		-- Non-Recursive CTE:
