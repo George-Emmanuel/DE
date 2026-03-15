7f6e7311672232d1aa5dafdabdacd9ade082a118
George Emmanuel <george444emmanuel@gmail.com>
2026-03-15 19:18:45 +0530

    Update March

--- Hands-on Exercise/SQL/UltimatSQL.sql
+++ Hands-on Exercise/SQL/UltimatSQL.sql
@@ -920,7 +920,7 @@ FROM Sales.Employees;
 
 	-- Syntax:
 		-- Non-Recursive CTE:
-			-- Standalone CTE:
+			-- Standalone CTE: A CTE that is defined and used within a single query.
 			-- Nested CTE:
 
 		-- Recursive CTE:
