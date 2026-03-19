1e472a8837ac9ebb7543a6828f0393d56063a638
George Emmanuel <george444emmanuel@gmail.com>
2026-03-19 14:37:42 +0530

    Update March

--- Hands-on Exercise/SQL/Ultimate_SQL.sql
+++ Hands-on Exercise/SQL/Ultimate_SQL.sql
@@ -1366,3 +1366,14 @@ FROM Sales.Employees;
 			END
 
 		-- Example 2:
+			CREATE TRIGGER trg_AfterInsertEmployee ON Sales.Employees
+			AFTER INSERT
+			AS
+			BEGIN
+				INSERT INTO Sales.EmployeeLogs (EmployeeID, LogMessage, LogDate)
+				SELECT
+					EmployeeID,
+					'New Employee Added =' + EmployeeID,
+					GETDATE()
+				FROM INSERTED
+			END
