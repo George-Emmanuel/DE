31e9f5d4ccb1de048ec0c8398320fa77220a5031
George Emmanuel <george444emmanuel@gmail.com>
2026-03-20 12:14:53 +0530

    Update March

--- Hands-on Exercise/SQL/Ultimate_SQL.sql
+++ Hands-on Exercise/SQL/Ultimate_SQL.sql
@@ -1451,12 +1451,12 @@ FROM Sales.Employees;
 
 | Aspect                 | Clustered Index                                                                                 | Non-Clustered Index                                                                  |
 | ---------------------- | ----------------------------------------------------------------------------------------------- | ------------------------------------------------------------------------------------ |
-|   Definition**         | Physically sorts and stores rows                                                                | Separate structure with pointers to the data                                         |
-|   Number of Indexes**  | One index per table                                                                             | Multiple indexes allowed                                                             |
-|   Read Performance**   | Faster                                                                                          | Slower                                                                               |
-|   Write Performance**  | Slower (due to potential row reordering)                                                        | Faster (physical order unaffected)                                                   |
-|   Storage Efficiency** | More storage-efficient                                                                          | Requires additional storage space                                                    |
-|   Use Case**           | - Unique column  <br> - Not frequently modified column  <br> - Improves range query performance | - Columns frequently used in search conditions and joins  <br> - Exact match queries |
+|   Definition           | Physically sorts and stores rows                                                                | Separate structure with pointers to the data                                         |
+|   Number of Indexes    | One index per table                                                                             | Multiple indexes allowed                                                             |
+|   Read Performance     | Faster                                                                                          | Slower                                                                               |
+|   Write Performance    | Slower (due to potential row reordering)                                                        | Faster (physical order unaffected)                                                   |
+|   Storage Efficiency   | More storage-efficient                                                                          | Requires additional storage space                                                    |
+|   Use Case             | - Unique column  <br> - Not frequently modified column  <br> - Improves range query performance | - Columns frequently used in search conditions and joins  <br> - Exact match queries |
 
 ******************************************************************************************************************************************************************************************************************/
 
