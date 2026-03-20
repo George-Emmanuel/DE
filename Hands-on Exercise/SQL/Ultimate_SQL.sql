f8bb0258eedda0fa6dcac4e43beeef1a115f7a31
George Emmanuel <george444emmanuel@gmail.com>
2026-03-20 18:04:58 +0530

    Update March

--- Hands-on Exercise/SQL/Ultimate_SQL.sql
+++ Hands-on Exercise/SQL/Ultimate_SQL.sql
@@ -1814,3 +1814,7 @@ Every index has a cost:
 		-- 	if avg_fragmentation_in_percent	< 10%       ===> No Action Required
 		--	if avg_fragmentation_in_percent 10% - 30%   ===> Need to REORGANIZE
 		--	if avg_fragmentation_in_percent > 30%       ===> Need to REBUILD
+
+		ALTER INDEX idx_CustomerName_Country ON Sales.Customers REBUILD
+
+		ALTER INDEX idx_CustomerName_Country ON Sales.Customers REORGANIZE
