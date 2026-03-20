986136f8625bf08aa7aee17a6b792e25731e3030
George Emmanuel <george444emmanuel@gmail.com>
2026-03-20 17:55:36 +0530

    Update March

--- Hands-on Exercise/SQL/Ultimate_SQL.sql
+++ Hands-on Exercise/SQL/Ultimate_SQL.sql
@@ -1808,4 +1808,3 @@ Every index has a cost:
 			ips.page_count AS PageCount
 		FROM sys.dm_db_index_physical_stats(DB_ID(), NULL, NULL, NULL, 'DETAILED') AS ips
 		JOIN sys.indexes AS i ON ips.object_id = i.object_id AND ips.index_id = i.index_id
-		WHERE ips.database_id = DB_ID() AND ips.avg_fragmentation_in_percent > 10
