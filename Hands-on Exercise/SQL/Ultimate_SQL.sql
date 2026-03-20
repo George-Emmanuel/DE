b3133faf07055b02a0b7aef25ac689e8dee68af9
George Emmanuel <george444emmanuel@gmail.com>
2026-03-20 12:40:50 +0530

    Update March

--- Hands-on Exercise/SQL/Ultimate_SQL.sql
+++ Hands-on Exercise/SQL/Ultimate_SQL.sql
@@ -1561,4 +1561,4 @@ WHERE Status = 'Active';
 
 /* *********************** !!!!!!!!!!! IMPORTANT IMPORTANT IMPORTANT !!!!!!!!!!! ************************
 
-Choosing the right index type is crucial for optimizing query performance and ensuring efficient data retrieval.
+Choosing the RIGHT INDEX for optimizing query performance and ensuring efficient data retrieval.
