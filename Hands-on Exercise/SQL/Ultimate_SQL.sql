9b73c0a401c4707253220f539618683acbea4b8c
George Emmanuel <george444emmanuel@gmail.com>
2026-03-20 13:00:35 +0530

    Update March

--- Hands-on Exercise/SQL/Ultimate_SQL.sql
+++ Hands-on Exercise/SQL/Ultimate_SQL.sql
@@ -1605,8 +1605,6 @@ What it does:
 	2. Wide keys
 		* Increases size of all non-clustered indexes
 
--------------------------------------------------------------------------------------
-
 ### Rule of thumb:
 
 > Pick a column that matches how data is read, not just the primary key.
