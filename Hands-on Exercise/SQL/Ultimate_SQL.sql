bcb55c6bf3dda7934909053cebf42bc1c23d6cc1
George Emmanuel <george444emmanuel@gmail.com>
2026-03-20 16:00:50 +0530

    Update March

--- Hands-on Exercise/SQL/Ultimate_SQL.sql
+++ Hands-on Exercise/SQL/Ultimate_SQL.sql
@@ -1748,7 +1748,6 @@ Every index has a cost:
 	-- Monitor Duplicate Indexes
 		-- A duplicate index is an index that has the same key columns as another index on the same table, 
 		-- but may differ in included columns or other properties.
-
 		SELECT
 			tbl.name AS Table_name,
 			col.name AS Column_name,
