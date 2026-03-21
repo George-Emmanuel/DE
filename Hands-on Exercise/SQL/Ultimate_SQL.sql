40170867d378e3918d1246c0c7a7eb86377c69e4
George Emmanuel <george444emmanuel@gmail.com>
2026-03-21 13:37:31 +0530

    Update March

--- Hands-on Exercise/SQL/Ultimate_SQL.sql
+++ Hands-on Exercise/SQL/Ultimate_SQL.sql
@@ -1857,6 +1857,17 @@ Every index has a cost:
 
 	-- Phases of Project / Indexing Strategy
 	--		1. Initial Indexing Strategy
-	--		2. Usage Patterns Indexing
+	--			In OLAP --> Try to optimize READ Performance by switching LARGE FREQUENTLY used tables to COLUMNSTORE
+	--			In OLTP --> Try to optimize WRITE Performance by creating CLUSTERED INDEX on Primary Keys
+	
+	--		2. Usage Patterns Indexing:
+				-- Prompt to get Usge Patters from the Queries:
+					Analyze the following SQL queries and generate a report on table and column usage statistics. For each table, provide:
+					* The total number of times the table is used across all queries.
+					* A breakdown of each column in the table, showing:
+						* The number of times each column appears.
+						* The primary purpose of the column's usage (e.g., filtering, joining, grouping, aggregating).
+					Sort the tables in descending order based on their total usage.
+
 	--		3. Scenario-Based Indexing
 	--		4. Monitoring & Maintainance
