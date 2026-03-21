0f1a67ad361adfdeed74e9b40dd3ed78d74fc641
George Emmanuel <george444emmanuel@gmail.com>
2026-03-21 14:19:09 +0530

    Update March

--- Hands-on Exercise/SQL/Ultimate_SQL.sql
+++ Hands-on Exercise/SQL/Ultimate_SQL.sql
@@ -1994,3 +1994,12 @@ Every index has a cost:
 			   (2, 102, '2025-07-20', 150.00),
 			   (3, 103, '2026-09-10', 300.00),
 			   (4, 104, '2027-11-25', 400.00)
+	-- Check in which partition the data is inserted
+		SELECT
+			p.partition_number AS PartitionNumber,
+			f.name AS PartitionFilegroup,
+			p.rows AS NumberOfRows
+		FROM sys.partitions p
+		JOIN sys.destination_data_spaces dds ON p.partition_number = dds.destination_id
+		JOIN sys.filegroups f ON dds.data_space_id = f.data_space_id
+		WHERE OBJECT_NAME(p.object_id) = 'Orders_Partitioned';
