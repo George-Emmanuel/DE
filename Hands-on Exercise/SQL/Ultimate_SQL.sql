811d8904b0785ced335dd09f095c5cc2cd3ed8fc
George Emmanuel <george444emmanuel@gmail.com>
2026-03-21 14:24:36 +0530

    Update March

--- Hands-on Exercise/SQL/Ultimate_SQL.sql
+++ Hands-on Exercise/SQL/Ultimate_SQL.sql
@@ -2003,3 +2003,4 @@ Every index has a cost:
 		JOIN sys.destination_data_spaces dds ON p.partition_number = dds.destination_id
 		JOIN sys.filegroups f ON dds.data_space_id = f.data_space_id
 		WHERE OBJECT_NAME(p.object_id) = 'Orders_Partitioned';
+-----------------------------------------------------------------------------------------------------------------------------------
