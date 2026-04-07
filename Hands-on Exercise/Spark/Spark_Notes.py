0defcaec750efee33f395d55e76e549346bca6d4
George Emmanuel <george444emmanuel@gmail.com>
2026-04-07 11:24:23 +0530

    Update April

---
+++ Hands-on Exercise/Spark/Spark_Notes.py
@@ -0,0 +1,13 @@
+'''
+1. What is Apache Spark (Core Idea)
+Spark is a distributed data processing engine.
+Instead of using one machine:
+It uses multiple machines (cluster).
+Data is:
+Split across machines
+Processed in parallel (in-memory)
+Combined into a final result
+
+Key takeaway:
+Parallel processing across multiple machines → faster big data processing.
+'''
